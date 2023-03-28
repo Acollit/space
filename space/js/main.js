efb8d090e4602519e0e22bfd6e0a546e648be7f2
Acolit <82833189+Xareks@users.noreply.github.com>
2023-03-28 22:07:38 +0400

    Add files via upload

---
+++ space/js/main.js
@@ -0,0 +1,6 @@
+$(function () {
+    $('.services__slider').slick({
+        prevArrow: '<button type="button" class="arrow arrow-left"><img src="images/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="arrow arrow-right"><img src="images/arrow-right.svg" alt=""></button>',
+    });
+});
